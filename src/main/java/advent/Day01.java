a0046d7d67c38e75820867167ff72b05eef8c8a7
soares-daniel <sedam.code@gmail.com>
2025-12-01 05:37:33 +0000

    Day1 part 2 optimized

--- src/main/java/advent/Day01.java
+++ src/main/java/advent/Day01.java
@@ -1,7 +1,6 @@
 package advent;
 
 import advent.runner.AbstractDay;
-import advent.runner.NotImplementedException;
 import advent.util.IOUtils;
 
 import java.util.List;
@@ -18,28 +17,7 @@ public class Day01 extends AbstractDay {
     }
 
     @Override
-        public String part1(String input) {
-            List<String> lines = IOUtils.toLines(input);
-            int position = 50;
-            int zeroCount = 0;
-
-            for (String line : lines) {
-                char direction = line.charAt(0);
-                int value = Integer.parseInt(line.substring(1));
-                if (direction == 'L') {
-                    position -= value;
-                } else if (direction == 'R') {
-                    position += value;
-                }
-                position = (position % 100 + 100) % 100;
-                if (position == 0) {
-                    zeroCount++;
-                }
-            }
-            return String.valueOf(zeroCount);
-        }
-
-    @Override public String part2(String input) {
+    public String part1(String input) {
         List<String> lines = IOUtils.toLines(input);
         int position = 50;
         int zeroCount = 0;
@@ -47,16 +25,44 @@ public class Day01 extends AbstractDay {
         for (String line : lines) {
             char direction = line.charAt(0);
             int value = Integer.parseInt(line.substring(1));
-            int step = (direction == 'L') ? -1 : 1;
-
-            for (int i = 0; i < value; i++) {
-                position += step;
-                position = (position % 100 + 100) % 100;
-                if (position == 0) {
-                    zeroCount++;
-                }
+            if (direction == 'L') {
+                position -= value;
+            } else if (direction == 'R') {
+                position += value;
+            }
+            position = (position % 100 + 100) % 100;
+            if (position == 0) {
+                zeroCount++;
             }
         }
+        return String.valueOf(zeroCount);
+    }
+
+    @Override
+    public String part2(String input) {
+        List<String> lines = IOUtils.toLines(input);
+        int position = 50;
+        int zeroCount = 0;
+
+        for (String line : lines) {
+            char dir = line.charAt(0);
+            int value = Integer.parseInt(line.substring(1));
+            int crossings, newPos;
+
+            if (dir == 'R') {
+                int total = position + value;
+                crossings = total / 100;
+                newPos = total % 100;
+            } else {
+                int total = position - value;
+                crossings = (value - position + 99) / 100;
+                newPos = (total % 100 + 100) % 100;
+            }
+
+            zeroCount += crossings;
+            position = newPos;
+        }
+
         return String.valueOf(zeroCount);
     }
 }
